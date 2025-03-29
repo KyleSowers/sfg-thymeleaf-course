8a54972bfe7f371ea84b6913a24f2abee3f0e0e5
Kyle Sowers <kyle.a.sowers@gmail.com>
2025-03-29 09:54:35 -0400

    Initial commit/push. Added index.html & product.html files.

---
+++ src/test/java/com/kylesowers/sfgthymeleafcourse/SfgThymeleafCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kylesowers.sfgthymeleafcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgThymeleafCourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
